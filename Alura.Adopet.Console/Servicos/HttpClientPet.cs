c13aa250458e82a6a29615dcee0b9d1ce1ffbb63
matheus matos da costa <matheus.costa@senac.br>
2025-05-29 16:34:34 -0300

    - Criando Testes Unitários

--- Alura.Adopet.Console/Servicos/HttpClientPet.cs
+++ Alura.Adopet.Console/Servicos/HttpClientPet.cs
@@ -5,12 +5,12 @@ using Alura.Adopet.Console.Modelos;
 
 namespace Alura.Adopet.Console.Servicos
 {
-    internal class HttpClientPet
+    public class HttpClientPet
     {
         private HttpClient client;
-        public HttpClientPet()
+        public HttpClientPet(string uri = "http://localhost:5057")
         {
-            client = ConfiguraHttpClient("http://localhost:5057");
+            client = ConfiguraHttpClient(uri);
         }
 
         HttpClient ConfiguraHttpClient(string url)
